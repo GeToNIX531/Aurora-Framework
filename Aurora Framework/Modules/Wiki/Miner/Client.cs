5692ed7a66400550f488936a05e50a63f3457dd2
Потапов Дмитрий <dpotapow1234@gmail.com>
2024-03-23 02:46:29 +0300

    Я нашёл интересную фичу.
    Хотел создать поисковик слов в словаре.
    А получил, результат, нахождения близкого по смыслу слова.
    Конечно, это баг, и конечно оно не правильно находит слова. Но результат интересный :)
    
    +Новый датамайнер, для вики

---
+++ Aurora Framework/Modules/Wiki/Miner/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Saver;
+using Modules.Wiki;
+
+namespace Aurora_Framework.Modules.Wiki.V1._0.Miner
+{
+    public class Client
+    {
+        private API api;
+        public Client()
+        {
+            api = new API();
+        }
+
+        public async Task<bool> Search(string Find, int Step = 0, int Max = 4)
+        {
+            if (Max == 0) return false; 
+
+            var urls = await api.GetUrls(Find);
+
+            bool find = false;
+            if (urls.Count > 0) find = true;
+
+            for (int i = 0; i < urls.Count; i++)
+            {
+                string title = urls.ElementAt(i).Key;
+                if (Find == title) continue;
+
+                if (api.TryGetPage(title, out var Page))
+                {
+                    string text = api.GetText(Page);
+                    Data data = new Data(Find, Step, i, text, title);
+                    data.Save($"Wiki/{data.Hash}.wiki", out var err);
+
+                    await Search(title, Step + 1, Max - 1);
+                }
+            }
+
+            return find;
+        }
+    }
+}
---
+++ Aurora Framework/Modules/Wiki/Miner/Forms/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aurora_Framework.Modules.Wiki.V1._0.Forms
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            client = new Miner.Client();
+        }
+
+        Miner.Client client;
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await client.Search(richTextBox1.Text);
+        }
+    }
+}
