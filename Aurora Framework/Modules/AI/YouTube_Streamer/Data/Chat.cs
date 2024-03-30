8a9e3522c2a95c3ff0f3017229761150656f1a2d
Потапов Дмитрий <dpotapow1234@gmail.com>
2024-03-31 00:53:48 +0300

    Подготовка

---
+++ Aurora Framework/Modules/AI/YouTube_Streamer/Data/Chat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurora_Framework.Modules.AI.YouTube_Streamer.Data
+{
+    public class Chat
+    {
+
+        public class User
+        {
+            public int ID;
+            public string NickName;
+        }
+
+        public class Message
+        {
+            public User User;
+            public string Massage;
+            public DateTime Time;
+        }
+    }
+}
---
+++ Aurora Framework/Modules/AI/YouTube_Streamer/Main.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Aurora_Framework.Modules.AI.YouTube_Streamer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(387, 387);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 498);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
