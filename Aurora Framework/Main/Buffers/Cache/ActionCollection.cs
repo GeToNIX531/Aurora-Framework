14058f58f9ca62b771eeeeedb8ecbbae0997a6ae
Потапов Дмитрий <dpotapow1234@gmail.com>
2024-03-31 00:48:38 +0300

    Добавлены новые хранилища данных: Кэш Action Коллекция

---
+++ Aurora Framework/Main/Buffers/Cache/ActionCollection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurora_Framework.Main.Buffers
+{
+    public class ActionCollection
+    {
+        Cache<ActionHandler> actions;
+        public int index;
+
+        public int Size { get; private set; }
+        private int startSize;
+        public ActionCollection(int Size)
+        {
+            actions = new Cache<ActionHandler>(Size);
+            index = 0;
+            this.Size = startSize = Size;
+        }
+
+        public void Add(ActionHandler Action, bool Opitimized = false)
+        {
+            if (actions.Add(Action) && Opitimized == false)
+                actions.AddSize(startSize);
+
+            if (Opitimized) actions.Optimized();
+        }
+
+        public Result LastResult;
+        public Result Next()
+        {
+            index++;
+            index = index % actions.Size;
+            var action = actions.Get(index);
+            LastResult = action.Invoke(this, LastResult);
+            return LastResult;
+        }
+
+        public delegate Result ActionHandler(ActionCollection Collection, Result BackResult = null);
+
+        public class Result
+        {
+            public bool OK;
+            public object[] Value;
+
+            public Result() => OK = true;
+            public Result(bool OK = true)
+            {
+                this.OK = OK;
+            }
+
+            public Result(params object[] Value)
+            {
+                this.Value = Value;
+                this.OK = true;
+            }
+
+            public Result(bool OK, params object[] Value)
+            {
+                this.OK = OK;
+                this.Value = Value;
+            }
+        }
+    }
+}
---
+++ Aurora Framework/Main/Buffers/Cache/Cache.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Aurora_Framework.Main.Buffers
+{
+    public class Cache<T>
+    {
+        private T[] Array;
+
+        public int Size { get; private set; }
+        public int index { get; private set; }
+
+        public Cache(int Size = 100)
+        {
+            this.Size = Size;
+            Array = new T[Size];
+
+            if (Size == 0)
+                throw new Exception($"{nameof(Cache<T>)} размер массива равен 0");
+        }
+
+        private string directory;
+        public void SetSave(string Directory)
+        {
+            directory = Directory;
+        }
+
+        public bool Add(T Element)
+        {
+            if (index == Size - 1)
+            {
+                Save();
+                Clear();
+                return false;
+            }
+            else
+            {
+                Array[index] = Element;
+                index++;
+                count++;
+
+                return index == Size - 1;
+            }
+        }
+
+        public T Get(int Index)
+        {
+            int index = Index % Size;
+
+            return Array[index];
+        }
+        public T Get(Random RND)
+        {
+            int index = RND.Next(0, this.index);
+            return Array[index];
+        }
+
+        long count;
+        public void Save()
+        {
+            string content = JsonConvert.SerializeObject(Array);
+
+            if (Directory.Exists(directory) == false)
+                Directory.CreateDirectory(directory);
+            string path = $"{directory}/{count}-{index}.json";
+
+            File.WriteAllText(path, content);
+        }
+
+        public void Clear()
+        {
+            for (index = 0; index < Size; index++)
+                Array[index] = default;
+            index = 0;
+        }
+
+        public void Optimized()
+        {
+            int count = index;
+            var array = new T[count];
+            for (int i = 0; i < count; i++)
+                array[i] = Array[i];
+
+            Array = array;
+            Size = count;
+        }
+
+        public void AddSize(int Value = 100)
+        {
+            int count = index;
+            int size = Size + Value;
+
+            T[] array = new T[size];
+            for (int i = 0; i < count; i++)
+                array[i] = Array[i];
+
+            Array = array;
+            Size = size;
+        }
+    }
+}
